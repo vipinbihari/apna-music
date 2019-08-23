ec05f0e481d2773b57bd81201eca918c5bc1cbae
VIPIN BIHARI <vipinbiharitiwari25@gmail.com>
2019-08-23 10:20:44 +0530

    Update autoplay.php

--- logic/autoplay.php
+++ logic/autoplay.php
@@ -4,11 +4,11 @@ if(isset($id)){
 $pageToken = $_GET['pageToken'];
 if(isset($pageToken)){
 
-$request = "https://www.googleapis.com/youtube/v3/search?pageToken={$pageToken}&part=id&relatedToVideoId={$id}&type=video&key=AIzaSyB2oMXNhlDjwemhqE7xaOYp1bpIB08Kx2E";
+$request = "https://www.googleapis.com/youtube/v3/search?pageToken={$pageToken}&part=id&relatedToVideoId={$id}&type=video&key=AIzaSyDFiLbujgQtWGIWQudc2q-BuuFJlzsxUEE";
 
 
 }else{
-$request = "https://www.googleapis.com/youtube/v3/search?part=id&relatedToVideoId={$id}&type=video&key=AIzaSyB2oMXNhlDjwemhqE7xaOYp1bpIB08Kx2E";
+$request = "https://www.googleapis.com/youtube/v3/search?part=id&relatedToVideoId={$id}&type=video&key=AIzaSyDFiLbujgQtWGIWQudc2q-BuuFJlzsxUEE";
 }
 echo file_get_contents($request);
 }
